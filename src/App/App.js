2fd2dcd57676316381114961a64c71aaa778f0a3
Monique.Bass01 <monique.bass01@gmail.com>
2020-05-18 22:00:48 -0500

    added auth

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,52 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import Auth from '../components/Auth/Auth';
+import Team from '../components/Team/Team';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
+
+import fbConnection from '../helpers/data/connection';
 import './App.scss';
 
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false, // the app loads, you are not authenticated
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+    const loadComponent = () => {
+      let componentToLoad = '';
+      if (authed) {
+        componentToLoad = <Team />;
+      } else {
+        componentToLoad = <Auth />;
+      }
+      return componentToLoad;
+    };
+
     return (
-      <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">Sports-Roster</button>
+        <div className="App">
+          <MyNavbar authed={authed}/>
+        <h2>Sports Roster</h2>
+        {loadComponent()}
       </div>
     );
   }
