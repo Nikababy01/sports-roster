4a529f098b738408e0b96112d1a6758b5e7eb0d2
Monique.Bass01 <monique.bass01@gmail.com>
2020-05-26 21:19:07 -0500

    added form

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -5,14 +5,16 @@ import playersData from '../../helpers/data/playersData';
 import authData from '../../helpers/data/authData';
 
 import Player from '../Player/Player';
+import TeamForm from '../TeamForm/TeamForm';
 
 
 class Team extends React.Component {
   state = {
     players: [],
+    formOpen: false,
   }
 
-  getInfo = () => {
+  getAllPlayers = () => {
     const { playersId } = this.props;
     playersData.getPlayersByUid(authData.getUid(playersId))
       .then((players) => this.setState({ players }))
@@ -20,22 +22,34 @@ class Team extends React.Component {
   }
 
   componentDidMount() {
-    this.getInfo();
+    this.getAllPlayers();
   }
 
   removePlayers = (playersId) => {
     playersData.deletePlayer(playersId)
-      .then(() => this.getInfo())
+      .then(() => this.getAllPlayers())
       .catch((err) => console.error('could not delete player: ', err));
   }
 
+  saveNewPlayer = (newPlayer) => {
+    playersData.savePlayer(newPlayer)
+      .then(() => {
+        this.getAllPlayers();
+        this.setState({ formOpen: false });
+      })
+      .catch((err) => console.error('unable to save player: ', err));
+  }
+
   render() {
-    const { players } = this.state;
+    const { players, formOpen } = this.state;
+
     const makePlayers = players.map((player) => <Player key={player.id} player={player} removePlayers={this.removePlayers}/>);
 
     return (
       <div className="Team">
         <h2 className="text-center">Starting Players</h2>
+        <button className="btn btn-warning" onClick={() => this.setState({ formOpen: true })}><i className="fas fa-plus"></i></button>
+        { formOpen ? <TeamForm saveNewPlayer={this.saveNewPlayer}/> : ''}
         <div className="d-flex flex-wrap">
           {makePlayers}
         </div>
